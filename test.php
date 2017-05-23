baa57b1e8c7e9bd471a9a198e592be6b3adfb583
sat2they <sat2they@GMAIL.COM>
2017-05-23 15:47:07 +0430

    new
    new

--- test.php
+++ test.php
@@ -1,5 +1,20 @@
-<?php
+<?php 
+define('BOT_TOKEN', '383689327:AAEAZuRKJ4_PVpWxh9W_BJ3-suTKYJQTdf8');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+	
+// read incoming info and grab the chatID
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+$chatID = $update["message"]["chat"]["id"];
+		
+// compose reply
+$reply =  sendMessage();
+		
+// send reply
+$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
+file_get_contents($sendto);
 
-
-echo "hi";
-?>
+function sendMessage(){
+$message = "I am a baby bot.";
+return $message;
+}
